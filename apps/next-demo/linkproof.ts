3ee62b3397d8e44a8eabf6df7143c07b884bc9df
James O'Halloran <james.p.ohalloran@gmail.com>
2022-11-11 22:16:23 -0400

    compile linkproof file

---
+++ apps/next-demo/linkproof.ts
@@ -0,0 +1,4 @@
+const foo = {
+  abc: 123,
+};
+export default foo;
--- packages/link-proofer/src/link-proofer.ts
+++ packages/link-proofer/src/link-proofer.ts
@@ -1,4 +1,35 @@
-export const foo = () => {
-  const j = "123";
-  return j;
+const fs = require("fs");
+import { build } from "esbuild";
+import path from "path";
+
+export const getLinkProofFile = async () => {
+  const linkproofFilename = "linkproof";
+
+  let hasTs = false;
+
+  if (fs.existsSync(path.join(process.cwd(), `${linkproofFilename}.ts`))) {
+    hasTs = true;
+  } else if (
+    fs.existsSync(!path.join(process.cwd(), `${linkproofFilename}.js`))
+  ) {
+    throw new Error(
+      `No ${linkproofFilename}.ts or linkproof.js file found in project root`
+    );
+  }
+
+  await build({
+    entryPoints: [
+      path.join(process.cwd(), `${linkproofFilename}.${hasTs ? "ts" : "js"}`),
+    ],
+    bundle: true,
+    platform: "node",
+    target: "node17",
+    outfile: path.join(process.cwd(), "dist", `${linkproofFilename}.out.js`),
+  });
+
+  const linkProofFile = await require(path.join(
+    process.cwd(),
+    "dist",
+    `${linkproofFilename}.out.js`
+  )).default;
 };
