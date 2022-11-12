03ab3836b33b15c8e6f375ade5209efb50a2c2dd
James O'Halloran <james.p.ohalloran@gmail.com>
2022-11-12 11:12:41 -0400

    Add commander

--- packages/link-proofer/src/index.ts
+++ packages/link-proofer/src/index.ts
@@ -1,5 +1,16 @@
 import { checkFiles } from "./link-proofer";
+import { Command } from "commander";
 
 export async function init(args: any) {
-  await checkFiles();
+  const program = new Command();
+
+  program
+    .name("linkproofer")
+    .description("CLI tool to audit links in your project")
+    .version("1.0.0")
+    .action(async (options) => {
+      await checkFiles();
+    });
+
+  program.parse(args);
 }
